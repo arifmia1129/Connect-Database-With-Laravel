61c444bc6d5eb0810e12963cb2019073e8098da2
mdarifmia <arif@supremesupports.com.au>
2024-01-18 21:16:28 +0600

    groupby aggregate

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -13,6 +13,20 @@ class EmployeeController extends Controller
     // echo 'Total salary: '. DB::table('employee')->sum('salary');
     // echo 'Average salary is: '.DB::table('employee')->avg('salary');
     // echo 'Maximum employee age is: '.DB::table('employee')->max('salary');
-    echo 'Minimum employee age is: '.DB::table('employee')->min('salary');
+    // echo 'Minimum employee age is: '.DB::table('employee')->min('salary');
+
+
+    $result = DB::table('employee')->select('designation', DB::raw('COUNT(*) as total_count'))->groupBy('designation')->get();
+
+    foreach($result as $item) {
+        $designation  = $item->designation;
+        $count = $item->total_count;
+
+        echo $designation .'-';
+        echo $count.'<br><br><br>';
+
+        // dd($item);
+    }
+
     }
 }
